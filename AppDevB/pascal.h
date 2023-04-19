30df539a4ed5b64ba7e85c3558a8dafbf20d73a0
Alina Zubko <e2203754@edu.vamk.fi>
2023-04-19 15:44:00 +0300

    Add project files.

---
+++ AppDevB/pascal.h
@@ -0,0 +1,10 @@
+#pragma once
+//a header fike is usually composed by 3 parts : 
+//1. constant definitions (either using #define or enuemration)
+//2. structure difinitions
+//3. function declarations 
+#define ROWS 10
+
+// dunction declarations 
+int factorial(int n);
+int binomial(int n, int k);
---
+++ AppDevB/screen.h
@@ -0,0 +1,7 @@
+// constant definition 
+
+enum COLORS { BLACK = 30, RED, GREEN, YELLOW, BLUE, CYAN, WHITE };
+
+void clearScreen(void);
+void setColors(int fg, int bg);
+void resetScreen(void);
